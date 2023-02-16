4cddf925f35ddac51ed77671ccfa9b4940677681
Igor Boiko <igorboiko@gmail.com>
2023-02-16 17:42:42 +0200

    01 add gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+// Вибираємо потрібне місце для нової розмітки
+const galleryContainer = document.querySelector('.gallery');
+// Створюємо нову розмітку галареї за шаблоном задачі
+const galeryMarkupString = galleryItems
+  .map(
+    (item) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`
+  )
+  .join('');
+// console.log(galeryMarkupString);
+galleryContainer.insertAdjacentHTML('beforeend', galeryMarkupString);
+// console.log(galleryContainer);
